ebe54ae533891e983f6822683b5733948c88ed3c
yhpgKIM <greenboy94@naver.com>
2020-09-21 17:48:01 +0900

    삭제 기능 구현

--- node.js/main.js
+++ node.js/main.js
@@ -54,7 +54,14 @@ var app = http.createServer(function(request,response){
             fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
             var title = queryData.id;
             var template = templateHTML(title, list, `<h2>${title}</h2>${description}`,
-            `<a href="/creat">creat</a> <a href="/update?id=${title}">update</a>`);
+            `
+            <a href="/creat">creat</a> 
+            <a href="/update?id=${title}">update</a> 
+            <form action="delete_process" method="post">
+                <input type="hidden" name="id" value="${title}">
+                <input type="submit" value="delete">
+            </form>
+            `);
             response.writeHead(200);
             response.end(template);
              });
@@ -109,7 +116,6 @@ var app = http.createServer(function(request,response){
         response.end(template);
          });
     });
-    
 } else if(pathname === '/update_process'){
     var body = '';
     request.on('data', function(data){
@@ -126,6 +132,19 @@ var app = http.createServer(function(request,response){
                 response.end();
             });
         })
+    });    
+} else if(pathname === '/delete_process'){
+    var body = '';
+    request.on('data', function(data){
+        body+=data;
+    });
+    request.on('end', function(){
+        var post = qs.parse(body);
+        var id = post.id;
+        fs.unlink(`data/${id}`, function(err){
+            response.writeHead(302, {Location: `/`});   
+            response.end();
+        });
     });    
 } else {
     response.writeHead(404);

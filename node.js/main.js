e4f199bb6a402ec1ae5b29048530f126cfd2764b
yhpgKIM <greenboy94@naver.com>
2020-09-21 16:43:13 +0900

    request 적용중

--- node.js/main.js
+++ node.js/main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function templateHTML(title, list, body){
 
@@ -14,6 +15,7 @@ function templateHTML(title, list, body){
     <body>
       <h1><a href="/">WEB</a></h1>
       ${list}
+      <a href="/creat">creat</a>
       ${body}
     </body>
     </html>
@@ -57,6 +59,31 @@ var app = http.createServer(function(request,response){
         });
     }
         
+} else if(pathname === '/creat'){
+    fs.readdir('./data', function(error, filelist){
+        var list = templatelist(filelist);
+        var title = 'Creat Post';      
+        var template = templateHTML(title, list, 
+            `<form action="http://localhost:3000/create_process" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p><textarea name="description" placeholder="description"></textarea></p>
+            <p><input type="submit"></p>
+            </form>
+            `);
+        response.writeHead(200);
+        response.end(template);
+    });
+} else if(pathname === '/create_process'){
+    var body = '';
+    request.on('data', function(data){
+        body+=data;
+    });
+    request.on('end', function(){
+        var post = qs.parse(body);
+        console.log(post);
+    });
+    response.writeHead(200);   
+    response.end('success');
 } else {
     response.writeHead(404);
     response.end('Not Found');

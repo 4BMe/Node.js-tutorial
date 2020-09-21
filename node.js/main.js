4e72cf5e20a388338250beadf52e44af225498bd
yhpgKIM <greenboy94@naver.com>
2020-09-21 20:08:09 +0900

    npm sanitize-html 보안 적용

--- node.js/main.js
+++ node.js/main.js
@@ -3,8 +3,9 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
+var path = require('path');
+var sanitizeHtml = require('sanitize-html'); 
 
- 
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
@@ -25,14 +26,19 @@ var app = http.createServer(function(request,response){
     } else {
         fs.readdir('./data', function(error, filelist){
             var list = template.list(filelist);
-            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+            var filteredId = path.parse(queryData.id).base;
+            fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
             var title = queryData.id;
-            var html = template.html(title, list, `<h2>${title}</h2>${description}`,
+            var sanitizedTitle = sanitizeHtml(title);
+            var sanitizedDescription = sanitizeHtml(description,{
+                allowedTags:['h1']
+            }); 
+            var html = template.html(title, list, `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
             `
-            <a href="/creat">creat</a> 
-            <a href="/update?id=${title}">update</a> 
+            <a href="/create">create</a> 
+            <a href="/update?id=${sanitizedTitle}">update</a> 
             <form action="delete_process" method="post">
-                <input type="hidden" name="id" value="${title}">
+                <input type="hidden" name="id" value="${sanitizedTitle}">
                 <input type="submit" value="delete">
             </form>
             `);
@@ -42,10 +48,10 @@ var app = http.createServer(function(request,response){
         });
     }
         
-} else if(pathname === '/creat'){
+} else if(pathname === '/create'){
     fs.readdir('./data', function(error, filelist){
         var list = template.list(filelist);
-        var title = 'Creat Post';      
+        var title = 'Create Post';      
         var html = template.html(title, list, 
             `<form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
@@ -72,7 +78,8 @@ var app = http.createServer(function(request,response){
     });
 } else if(pathname === '/update'){
     fs.readdir('./data', function(error, filelist){
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+        var filteredId = path.parse(queryData.id).base;
+        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
         var title = queryData.id;
         var list = template.list(filelist);
         var html = template.html(title, list,
@@ -84,7 +91,7 @@ var app = http.createServer(function(request,response){
             <p><input type="submit"></p>
             </form>
             `,
-            `<a href="/creat">creat</a> <a href="/update?id=${title}">update</a>`
+            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
             );
         response.writeHead(200);
         response.end(html);
@@ -115,7 +122,8 @@ var app = http.createServer(function(request,response){
     request.on('end', function(){
         var post = qs.parse(body);
         var id = post.id;
-        fs.unlink(`data/${id}`, function(err){
+        var filteredId = path.parse(id).base;
+        fs.unlink(`data/${filteredId}`, function(err){
             response.writeHead(302, {Location: `/`});   
             response.end();
         });

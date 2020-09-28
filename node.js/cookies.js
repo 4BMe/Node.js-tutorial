f4f65e85daba1129d89a83707d6b4c3c57745313
yhpgKIM <greenboy94@naver.com>
2020-09-28 13:35:39 +0900

    쿠키 설정

--- node.js/cookies.js
+++ node.js/cookies.js
@@ -11,8 +11,12 @@ http.createServer(function(request, response){
         'Set-Cookie':[
             'yummy_cookie=choco', 
             'tasty_cookie=strawberry',
-            `Permanent=cookies; Max-Age=${60*60*24*30}`
+            `Permanent=cookies; Max-Age=${60*60*24*30}`,
+            'Secure=Secure; Secure',
+            'HttpOnly=HttpOnly; HttpOnly',
+            'Path=Path; Path=/cookie',
+            'Doamin=Domain; Domain=test.o2.org'
         ]
     });
     response.end('Cookie!!');
-}).listen(4000);
+}).listen(3000);
